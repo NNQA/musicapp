e11b11e6cfcb0e4199d8922521b74508ccc79e94
NNQA <anhla12h@gmail.com>
2023-03-06 13:50:20 +0700

    install nextjs, tailwindcss

---
+++ music-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+
+
+export default function Home() {
+  return (
+    <h1 className='text-red-900'>
+      Hello world!
+    </h1>
+  );
+}
