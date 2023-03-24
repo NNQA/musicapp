c22cca2aad2da77a4d6726c8dbc538118120d12d
NNQA <anhla12h@gmail.com>
2023-03-24 20:12:54 +0700

    feat: add startui

--- music-app/src/pages/index.tsx
+++ music-app/src/pages/index.tsx
@@ -2,16 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import StartUi from '@/components/elements/StartUi/StartUi'
 import Link from 'next/link'
+import FormMain from './Home/FormMain'
+import StartUi from './StartUi/StartUi'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <StartUi />
-    // <Link href="/posts/[id]">
-    //   asdasd
-    // </Link>
   )
 }
