0f05a77e0a53ed05c611b32f611580bea00669d1
NNQA <anhla12h@gmail.com>
2023-04-21 09:14:46 +0700

    add getUserCurrent

--- music-app/src/hook/currentuser.ts
+++ music-app/src/hook/currentuser.ts
@@ -3,7 +3,7 @@ import fetcher from '@/server/fetcher'
 
 
 const userCurrent = () => {
-    const {data, error, isLoading} = userSWR('/api/current', fetcher);
+    const {data, error, isLoading} = userSWR('/api/getCurrentUser', fetcher);
     return {
         data,
         error,
