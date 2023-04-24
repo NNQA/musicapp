1ffc1e2b5c847d97a733599204b2721b3070c8bb
NNQA <anhla12h@gmail.com>
2023-04-24 20:27:04 +0700

    fix file config

--- music-app/.history/next.config_20230422192802.js
+++ music-app/.history/next.config_20230422192802.js
@@ -9,13 +9,10 @@ module.exports = {
     appDir: true,
     typedRoutes: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   disableExperimentalFeaturesWarning: true,
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ['res.cloudinary.com'],
   },
   webpack(config, options) {
     const { isServer } = options;
@@ -24,13 +21,13 @@ module.exports = {
       exclude: config.exclude,
       use: [
         {
-          loader: require.resolve("url-loader"),
+          loader: require.resolve('url-loader'),
           options: {
             limit: config.inlineImageLimit,
-            fallback: require.resolve("file-loader"),
+            fallback: require.resolve('file-loader'),
             publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? "../" : ""}static/images/`,
-            name: "[name]-[hash].[ext]",
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]',
             esModule: config.esModule || false,
           },
         },
