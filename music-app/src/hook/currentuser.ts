fa74f5ac0090f7d82ff229b1a4a56b7e00cd321b
NNQA <anhla12h@gmail.com>
2023-05-02 17:49:40 +0700

    change hook with get current user

--- music-app/src/hook/currentuser.ts
+++ music-app/src/hook/currentuser.ts
@@ -1,13 +1,23 @@
-import userSWR from 'swr'
-import fetcher from '@/server/fetcher'
-
+import useSWR from "swr";
+import fetcher from "@/server/fetcher";
+import { useSession } from "next-auth/react";
 
 const userCurrent = () => {
-    const {data, error, isLoading} = userSWR('/api/getCurrentUser', fetcher);
-    return {
+    const { data, error, isLoading, mutate } = useSWR(
+        "/api/getCurrentUser",
+        fetcher
+      );
+      const session = useSession().data;
+      
+      if (!session) {
+        return { data: null, error: null, isLoading: true, mutate };
+      }
+      return {
         data,
         error,
-        isLoading
-    }
+        isLoading,
+        mutate,
+      };
 };
+
 export default userCurrent;
