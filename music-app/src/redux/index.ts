e24b72a6e429ec8a19fb0b6a09ea4c511ac5a153
NNQA <anhla12h@gmail.com>
2023-05-02 17:51:07 +0700

    refactor redux

--- music-app/src/redux/index.ts
+++ music-app/src/redux/index.ts
@@ -1,39 +1,10 @@
-import { legacy_createStore as createStore} from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+import audioPlayerReducer from './reducer';
 
-// Define the shape of your application state
-interface AppState {
-  currentIndex: number;
-}
-
-// Define the actions that can modify the state
-interface SetCurrentIndexAction {
-  type: 'SET_CURRENT_INDEX';
-  payload: {
-    index: number;
-  };
-}
-
-type AppAction = SetCurrentIndexAction;
-
-// Define the initial state
-const initialState: AppState = {
-  currentIndex: 0,
-};
-
-// Define the reducer function
-function reducer(state = initialState, action: AppAction): AppState {
-  switch (action.type) {
-    case 'SET_CURRENT_INDEX':
-      return {
-        ...state,
-        currentIndex: action.payload.index,
-      };
-    default:
-      return state;
-  }
-}
-
-// Create the store
-const store = createStore(reducer);
+const store = configureStore({
+  reducer: {
+    audioPlayer: audioPlayerReducer,
+  },
+});
 
 export default store;
