220783f13ac068fabdea7a83ee2a981b5048175a
NNQA <anhla12h@gmail.com>
2023-05-08 03:42:31 +0700

    feat: add like

---
+++ music-app/src/pages/api/like/getLikedId.ts
@@ -0,0 +1,46 @@
+import { prisma } from "@/server/prisma";
+
+export default async function getLiked(req: any, res: any) {
+  if (req.method === "GET") {
+    const {sId, uId} = req.query
+    const data = await prisma.like.findMany({
+      where: {
+        songId:sId,
+        userId: uId
+      },
+      include:{ 
+        song: true,
+        user:true,
+      }
+      
+    });
+    if (data.length === 0) {
+      console.log("adasdasdasd")
+      return res.status(200).json({ message: 'notLike' });
+    }
+    console.log("123")
+    return res.status(200).json(data);
+  } else if (req.method === "POST") {
+    const { ids, idu } = req.body;
+    console.log(req.body);
+    const data = await prisma.like.create({
+      data: {
+        songId: ids,
+        userId: idu,
+      },
+    });
+    return res.status(200).json(data);
+  } else if (req.method === "DELETE") {
+    const { ids, idu } = req.query;
+    console.log(ids +" " + idu)
+    const data = await prisma.like.deleteMany({
+      where: {
+        songId: ids,
+        userId: idu,
+      },
+    });
+    return res.status(200).json(data);
+  } else {
+    res.status(400).json({ message: "Invalid method" });
+  }
+}
